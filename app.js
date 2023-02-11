424a7be3b97a84387999fcd1a70450907f0de2cf
sandip <dahalsandip97@gmail.com>
2023-02-11 16:24:16 +0545

    updated js file

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-const yourName = document.querySelector("#yourName")
-const theirName = document.querySelector("#theirName")
+let yourName = document.querySelector('#yourName')
+let theirName = document.querySelector('#theirName')
 document.querySelector('#calc').addEventListener('click', calculate)
 const output = document.querySelector('#output')
-const singleGuys = ['madhu','hari','ram']
-function calculate(){
-    const yourNamevalue =yourName.value
-    const theirNamevalue = theirName.value
-    if (yourNamevalue == '' || theirNamevalue == ''){
-        return alert( 'please entre both name')
-    }
-    if (singleGuys.includes(yourNamevalue.toLowerCase()) || singleGuys.includes (theirNamevalue.toLowerCase())) {
-         output.innerText ='oops this guy is play boy'}
-         clearInput()
-         return
-    
-    // console.log(yourNamevalue,theirNamevalue)
-    const lovepercent =  parseInt(Math.random()*100)
-    // console.log(lovepercent)
-    output.innerText = yourNamevalue + ' and ' + theirNamevalue + ' are in '+ lovepercent + "% love"
-    clearInput()
+
+
+function clearInput() {
+    yourName.value = ''
+    theirName.value = ''
 }
-function clearInput(){
-    yourName.value = ""
-    theirName.value=""
+const singleGuys = ['salman', 'hari', 'ram']
+function calculate() {
+    const yourNameValue = yourName.value
+    const theirNameValue = theirName.value
+
+    if (yourNameValue == '' || theirNameValue == '') {
+        clearInput()
+        return alert('Please enter both names')
+    }
+    if (yourNameValue.toLowerCase() == "sandip" && theirNameValue.toLowerCase() == "xxx") {
+        clearInput()
+        return output.innerText = "sandip loves xxx 100 %"
+    }
+    else if (singleGuys.includes(yourNameValue.toLowerCase()) || singleGuys.includes(theirNameValue.toLowerCase())) {
+        clearInput()
+        return output.innerText = "Single"
+    }
+    const lovePercent = parseInt(Math.random() * 100)
+    console.log(lovePercent)
+    output.innerText = yourNameValue + " and " + theirNameValue + " are in " + lovePercent + " % love "
+
+    clearInput()
 }
